d0e4b99c80556305dfdc462f428cd05157871781
LowGinWee <notginwee@gmail.com>
2019-07-01 17:16:33 +0800

    Demo 23

---
+++ Unit 2/Demo23/Assets/Scenes/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private float speed = 10.0f;
+    public Rigidbody rb;
+    public int score = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Coin")
+        {
+            score += 1;
+            Destroy(other.gameObject);
+        }
+    }
+}
+
+
