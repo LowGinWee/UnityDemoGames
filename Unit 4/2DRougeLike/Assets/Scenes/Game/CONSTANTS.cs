63e8df7317ffc153318c07b35ff7362c55f5bcff
LowGinWee <notginwee@gmail.com>
2019-07-11 15:22:56 +0800

    demo 41

---
+++ Unit 4/2DRougeLike/Assets/Scenes/Game/CONSTANTS.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CONSTANTS 
+{
+    public const int MAP_WIDTH = 15;
+    public const int MAP_HEIGHT = 10;
+}
+
+
