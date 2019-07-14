f16e2c3dbed2395971c8d22cd7de7759fb0d0e8f
LowGinWee <notginwee@gmail.com>
2019-07-14 16:24:28 +0800

    demos for unit 4
    to add unit 5

---
+++ Unit 4/2DRougeLike/Assets/Scenes/Game/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public enum PLAYER_STATES
+    {
+        IDLE,
+        MOVING,
+        DEAD
+    }
+
+    public PLAYER_STATES playerState;
+
+    private Vector2 targetPos;
+    private Vector2 currentPos;
+    void Start()
+    {
+        playerState = PLAYER_STATES.IDLE;
+        targetPos = new Vector2(0, 0.7f);
+        currentPos = targetPos;
+        this.gameObject.transform.position = targetPos;
+    }
+
+    void Update()
+    {
+        if (playerState == PLAYER_STATES.IDLE) MoveUser();
+    }
+
+    private void MoveUser()
+    {
+        Vector2 add = new Vector2((Input.GetKey("left") ? -1 : 0) + (Input.GetKey("right") ? 1 : 0), (Input.GetKey("up") ? 1 : 0) + (Input.GetKey("down") ? -1 : 0));
+        targetPos += add;
+
+        if (add.x == -1)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if (add.x == 1)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+        StartCoroutine(MoveToPosition(1.0f));
+    }
+
+    private IEnumerator MoveToPosition(float timeToMove)
+    {
+        playerState = PLAYER_STATES.MOVING;
+        float t = 0.0f;
+        do
+        {
+            currentPos = this.gameObject.transform.position;
+            t += Time.deltaTime / timeToMove;
+            this.gameObject.transform.position = Vector2.MoveTowards(this.gameObject.transform.position, targetPos, t);
+            yield return null;
+        } while (t < 1 && currentPos != targetPos);
+        playerState = PLAYER_STATES.IDLE;
+    }
+
+}
+
+
---
+++ Unit 4/demo/Assets/Scenes/Game/GameController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject floor;
+    public Sprite[] spriteFloor;
+    public GameObject playArea;
+    public int mapWidth;
+    public int mapHeight;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mapWidth = CONSTANTS.MAP_WIDTH;
+        mapHeight = CONSTANTS.MAP_HEIGHT;
+        SpawnMap();
+    }
+
+    public void CreateLevel()
+    {
+    }
+
+    public void NewLevel()
+    {
+    }
+
+
+    private void SpawnMap()
+    {
+        Vector2 pos = new Vector2(0, 0);
+        for (int i = 0; i < mapWidth; i++)
+        {
+            for (int j = 0; j < mapHeight; j++)
+            {
+                pos = new Vector2(i, j); ;
+                InstantiatePanel(playArea.transform, floor, pos, spriteFloor[Random.Range(0, spriteFloor.Length)]);
+            }
+        }
+    }
+
+    private void InstantiatePanel(Transform parent, GameObject panel, Vector2 pos, Sprite sprite)
+    {
+        GameObject i = Instantiate(panel);
+        i.GetComponent<SpriteRenderer>().sprite = sprite;
+        i.transform.position = pos;
+        i.transform.SetParent(parent);
+    }
+
+}
