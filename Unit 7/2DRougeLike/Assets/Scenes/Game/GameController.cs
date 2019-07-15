eee8395310199c84a03b1f5e4fc3c41a3cd3d5c3
LowGinWee <notginwee@gmail.com>
2019-07-15 12:17:45 +0800

    unit 7 demo 72

---
+++ Unit 7/2DRougeLike/Assets/Scenes/Game/GameController.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public enum GAMESTATE
+    {
+        START,
+        PAUSE,
+        WIN,
+        LOSE
+    }
+
+    public static GAMESTATE gameState;
+
+    public GameObject floor;
+    public Sprite[] spriteFloor;
+    public GameObject playArea;
+
+    public GameObject enemy;
+
+    public static int mapWidth;
+    public static int mapHeight;
+    public int enemyCount;
+
+    public List<Vector2> obstaclePos = new List<Vector2>();
+
+    public GameObject key;
+    public GameObject flask;
+
+    void Start()
+    {
+        gameState = GAMESTATE.START;
+
+        obstaclePos = new List<Vector2>();
+
+        mapWidth = CONSTANTS.MAP_WIDTH;
+        mapHeight = CONSTANTS.MAP_HEIGHT;
+        enemyCount = CONSTANTS.ENEMY_COUNT;
+
+        SpawnMap();
+        SpawnObject(key, 3);
+        SpawnObject(flask, 2);
+        SpawnEnemy(enemyCount);
+    }
+
+    public void CreateLevel()
+    {
+    }
+
+    public void NewLevel()
+    {
+    }
+
+
+    private void SpawnMap()
+    {
+        Vector2 pos = new Vector2(0, 0);
+        for (int i = 0; i < mapWidth; i++)
+        {
+            for (int j = 0; j < mapHeight; j++)
+            {
+                pos = new Vector2(i, j); ;
+                InstantiatePanel(playArea.transform, floor, pos, spriteFloor[Random.Range(0, spriteFloor.Length)]);
+            }
+        }
+    }
+
+    private void InstantiatePanel(Transform parent, GameObject panel, Vector2 pos, Sprite sprite)
+    {
+        GameObject i = Instantiate(panel);
+        i.GetComponent<SpriteRenderer>().sprite = sprite;
+        i.transform.position = pos;
+        i.transform.SetParent(parent);
+    }
+
+
+    private void SpawnObject(GameObject cloneThis, int num)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            Vector2 pos;
+            do
+            {
+                pos = new Vector2(Random.Range(1, mapWidth), Random.Range(1, mapHeight));
+            } while (!CheckObstaclePos(pos));
+            GameObject o = Instantiate(cloneThis);
+            o.transform.position = pos;
+            o.SetActive(true);
+            o.name = cloneThis.name;
+            o.transform.SetParent(playArea.transform);
+            obstaclePos.Add(pos);
+        }
+    }
+
+    private bool CheckObstaclePos(Vector2 pos)
+    {
+        foreach (Vector2 i in obstaclePos)
+        {
+            if (i == pos)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void SpawnEnemy(int num)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            GameObject e = Instantiate(enemy);
+            e.SetActive(true);
+            e.transform.SetParent(playArea.transform);
+            Enemy.TYPE type = (Enemy.TYPE)Random.Range(0, 3);
+            e.GetComponent<Enemy>().SetEnemy(new Vector2(Random.Range(0, mapWidth), Random.Range(0, mapHeight)), type);
+        }
+    }
+}
---
+++ Unit 7/Demo 71/Assets/Scenes/Game/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public enum TYPE
+    {
+        Zombie, // = 0
+        Ogre, // = 1
+        Demon // = 2
+    }
+
+    public TYPE type;
+
+    public Sprite[] sprites;
+
+    public void SetEnemy(Vector2 pos, TYPE type)
+    {
+        this.type = type;
+        this.gameObject.transform.position = pos;
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = sprites[(int)type];
+        this.gameObject.name = "Enemy";
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
